10ac13fbd846958d95675daa7e7d82a6f38f0c56
Sakib Rahman <seu.sakib@gmail.com>
2023-04-12 00:43:33 +0600

    added hire me spinner

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./Layout";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Layout></Layout>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+    </>
+  );
 }

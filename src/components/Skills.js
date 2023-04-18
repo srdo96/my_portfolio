086f6bc1049e8f0c9d95462ebdae405bf84fb1aa
Sakib Rahman <seu.sakib@gmail.com>
2023-04-18 15:51:07 +0600

    add eslint rules

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -24,8 +24,11 @@ const Skills = () => {
       {/* <h2 className="font-bold text-6xl mt-64 w-full text-center ">Skills</h2> */}
       <p className="font-medium">Here is the list of skill I know well.</p>
       <div className=" mt-4 ">
-        {techs.map((tech) => (
-          <div className=" inline-block m-2 px-3 border-2 border-solid rounded-lg  border-purple-600 font-medium uppercase text-center text-cyan-600">
+        {techs.map((tech, index) => (
+          <div
+            key={index}
+            className=" inline-block m-2 px-3 border-2 border-solid rounded-lg  border-purple-600 font-medium uppercase text-center text-cyan-600"
+          >
             {tech}
           </div>
         ))}
